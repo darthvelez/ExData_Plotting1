432ccaf5cfbcfa1f430d48e68b20b80d874a1231
darthvelez <carlosmvh@gmail.com>
2016-08-28 13:25:27 +0200

    Add files via upload

--- Plot2.R
+++ Plot2.R
@@ -6,11 +6,11 @@ if(!file.exists("exdata-data-household_power_consumption.zip")) {
 }
 
 #Subsetting the Data#
-  data <- read.table("household_power_consumption.txt", header=T, sep=";", na.strings = "?", stringsAsFactors = FALSE)
-  Global_active_power <- as.numeric(data$Global_active_power)
-  timeseries <- strptime(paste(data$Date, data$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
-  subdata <- data[(data$Date=="2007-2-1") | (data$Date=="2007-2-2"),]
-
+  hpc <- read.table("household_power_consumption.txt", header=T, sep=";", na.strings = "?", stringsAsFactors = FALSE)
+  hpc_subset <- hpc[(hpc$Date=="1/2/2007") | (hpc$Date=="2/2/2007"),]
+  Global_active_power <- as.numeric(hpc_subset$Global_active_power)
+  timeseries <- strptime(paste(hpc_subset$Date, hpc_subset$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
+     
 #Plot the plotting plotted plot#
   png("plot2.png", width=480, height=480)
   plot(timeseries, Global_active_power, type="l", xlab="", ylab="Global Active Power (kilowatts)")
